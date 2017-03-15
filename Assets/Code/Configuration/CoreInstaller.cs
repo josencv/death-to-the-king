61587d965dc4f16e45681992b692d1b8214b491d
José Ignacio Navarro <josencv@gmail.com>
2017-03-14 21:50:26 -0300

    Added sound manager 2D

---
+++ Assets/Code/Configuration/CoreInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Code.Infrastructure.Controller;
+using Assets.Code.Infrastructure.Sound;
+using Zenject;
+
+namespace Assets.Code.Configuration
+{
+    public class CoreInstaller : MonoInstaller  
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<IInputManager>().To<InputManager>().AsSingle();
+            Container.Bind<IGameInput>().To<GameInput>().AsTransient();
+            Container.Bind<ISoundManager>().To<SoundManager2D>().AsSingle();
+            Container.Bind<ISoundManager>().To<SoundManager2D>().AsSingle();
+        }
+    }
+}
---
+++ Assets/Code/Infrastructure/Sound/ISoundManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Infrastructure.Sound
+{
+    public interface ISoundManager
+    {
+        void PlayOneShotSound(AudioClip clip, float volume = 1.0F);
+        void PlayOneShotSoundRandom(AudioClip[] list, float volume = 1.0F);
+        void StopAllOneShotSounds();
+        void playLoopingSound(AudioClip clip, string soundID, int priority, float volume = 1.0F);
+        void StopLoopingSound(string soundID);
+        void StopAllLoopingSounds();
+    }
+}
