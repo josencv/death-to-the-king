a73896e8814d35eb046a92636f54f5837e0384a8
José Ignacio Navarro <josencv@gmail.com>
2017-04-24 22:39:35 -0300

    Added terrain. Changed character controller to rigidbody and capsule collider. Other fixes.

---
+++ Assets/Code/Components/Test/Test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Components.Test
+{
+    class Test : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
