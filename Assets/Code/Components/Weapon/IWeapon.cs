78172348043da8723e2bb05e9be916626bd4961d
José Ignacio Navarro <josencv@gmail.com>
2017-05-14 21:35:15 -0400

    Added attack component

---
+++ Assets/Code/Components/Weapon/IWeapon.cs
@@ -0,0 +1,7 @@
+﻿namespace Assets.Code.Components.Weapon
+{
+    public interface IWeapon
+    {
+        void Attack();
+    }
+}
---
+++ Assets/Code/Components/Weapon/MeleeWeapon.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Code.Constants;
+using Assets.Code.Infrastructure.Unity;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Code.Components.Weapon
+{
+    public class MeleeWeapon : MonoBehaviourExtension, IWeapon
+    {
+        private Animator animator;
+
+        [Inject]
+        private void Inject(Animator animator)
+        {
+            this.animator = animator;
+        }
+
+        public void Attack()
+        {
+            animator.SetTrigger(AnimatorParameters.Attack);
+        }
+    }
+}
