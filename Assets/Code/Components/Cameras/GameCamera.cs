f6a4dd8c89d34bb9eb5cd223a2b169a7a163f033
Rafael Lecaros <rafael.lecaros@gmail.com>
2017-06-02 03:01:16 -0300

    Following one player with slight jitter and so on

--- Assets/Code/Components/Cameras/GameCamera.cs
+++ Assets/Code/Components/Cameras/GameCamera.cs
@@ -5,9 +5,11 @@ public class GameCamera : MonoBehaviour
 {
     public GameObject target;       //Public variable to store a reference to the player game object
     public Vector3 startingOffset;         //Private variable to store the offset distance between the player and camera
-    public Vector3 distanceFromCenter;
-    public float leash = 2f;           //Threshold the player must move out for the camera to move
+    public float distanceFromCenter;
+    public float leashLength = 2f;           //Threshold the player must move out for the camera to move
     public bool isPullingLeash;
+    public Vector3 center;
+    public float damping = 1;
 
 
     // Use this for initialization
@@ -15,19 +17,28 @@ public class GameCamera : MonoBehaviour
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         transform.LookAt(target.transform);
+        center = transform.position;
         startingOffset = transform.position - target.transform.position; //Need to center the camera view in the target, instead of only storing the offset.
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        distanceFromCenter = transform.position - target.transform.position;
-        if (Mathf.Abs(distanceFromCenter.x) >= leash) isPullingLeash = true;
-        if (Mathf.Abs(distanceFromCenter.x) < leash) isPullingLeash = false;
+        distanceFromCenter = Vector3.Distance(center, target.transform.position);
+        if (Mathf.Abs(distanceFromCenter) >= leashLength) isPullingLeash = true;
+        if (Mathf.Abs(distanceFromCenter) < leashLength) isPullingLeash = false;
         if (isPullingLeash)
         {
-            // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-            transform.position = target.transform.position + startingOffset;
+            // Put the center behind the character leashing
+            if (center.x < target.transform.position.x) center = target.transform.position - new Vector3(leashLength, 0, 0);
+            if (center.x > target.transform.position.x) center = target.transform.position + new Vector3(leashLength, 0, 0);
+            if (center.z < target.transform.position.z) center = target.transform.position - new Vector3(0, 0, leashLength);
+            if (center.z > target.transform.position.z) center = target.transform.position + new Vector3(0, 0, leashLength);
+
+            // The camera now follows the center
+            Vector3 desiredPosition = center + startingOffset;
+            Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+            transform.position = position;
         }
     }
 }
