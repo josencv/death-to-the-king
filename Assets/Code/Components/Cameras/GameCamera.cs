f65b2fb1883fa9a0c2e61be4be806e3ec95c7534
Rafael Lecaros <rafael.lecaros@gmail.com>
2017-06-14 21:08:04 -0300

    safety  coimmit

--- Assets/Code/Components/Cameras/GameCamera.cs
+++ Assets/Code/Components/Cameras/GameCamera.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class GameCamera : MonoBehaviour
 {
@@ -8,34 +9,53 @@ public class GameCamera : MonoBehaviour
     public float distanceFromCenter;
     public float leashLength = 2f;           //Threshold the player must move out for the camera to move
     public bool isPullingLeash;
-    public Vector3 center;
     public float damping = 1;
+    public Bounds boundaries;
+
+    //private Vector3 centerFrom(Transform[] elements)
+    //{
+    //    Vector3[] positions = elements.Select(item => item.position).ToArray<Vector3>;
+    //    return new Vector3(0, 0, 0);
+    //} 
 
     // Use this for initialization
     void Start()
     {
+        boundaries = new Bounds();
+        boundaries.center = transform.position;
+        boundaries.Expand(leashLength*2);
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         transform.LookAt(target.transform);
-        center = transform.position;
         startingOffset = transform.position - target.transform.position; //Need to center the camera view in the target, instead of only storing the offset.
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(boundaries.center, boundaries.size);
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        distanceFromCenter = Vector3.Distance(center, target.transform.position);
+        distanceFromCenter = Vector3.Distance(boundaries.center, target.transform.position);
         if (Mathf.Abs(distanceFromCenter) >= leashLength) isPullingLeash = true;
         if (Mathf.Abs(distanceFromCenter) < leashLength) isPullingLeash = false;
+
+        //bool isIntersecting = boundaries.Intersects(target.GetComponent<Collider>().bounds);
         if (isPullingLeash)
         {
-            // Put the center behind the character leashing
-            if (center.x < target.transform.position.x) center = target.transform.position - new Vector3(leashLength, 0, 0);
-            if (center.x > target.transform.position.x) center = target.transform.position + new Vector3(leashLength, 0, 0);
-            if (center.z < target.transform.position.z) center = target.transform.position - new Vector3(0, 0, leashLength);
-            if (center.z > target.transform.position.z) center = target.transform.position + new Vector3(0, 0, leashLength);
+            //// Put the center behind the character leashing
+            //if (boundaries.center.x < target.transform.position.x) boundaries.center = target.transform.position - new Vector3(leashLength, 0, 0);
+            //if (boundaries.center.x > target.transform.position.x) boundaries.center = target.transform.position + new Vector3(leashLength, 0, 0);
+            //if (boundaries.center.z < target.transform.position.z) boundaries.center = target.transform.position - new Vector3(0, 0, leashLength);
+            //if (boundaries.center.z > target.transform.position.z) boundaries.center = target.transform.position + new Vector3(0, 0, leashLength);
+
+            Vector3 tail = target.transform.forward * -1 * leashLength; // only works (and badly at that) for one target
+            boundaries.center = target.transform.position + tail;
 
             // The camera now follows the center
-            Vector3 desiredPosition = center + startingOffset;
+            Vector3 desiredPosition = boundaries.center + startingOffset;
             Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
             transform.position = position;
         }
